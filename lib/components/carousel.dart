7fffd3a644a4e58127163597f0d5215cb12c4b93
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-03-14 15:55:38 +0530

    carousel updates

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -1,3 +1,4 @@
+import 'package:clickandcut/colors.dart';
 import 'package:flutter/material.dart';
 
 class Carousel extends StatefulWidget {
@@ -11,10 +12,23 @@ class _CarouselState extends State<Carousel> {
   @override
   Widget build(BuildContext context) {
     return Container(
+        height: 320,
         child: PageView.builder(
             itemCount: 5,
             itemBuilder: (context, position) {
               return _buildPageItem(position);
             }));
+  }
+
+  Widget _buildPageItem(int Index) {
+    return Container(
+      height: 220,
+      margin: EdgeInsets.only(left: 5, right: 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: Index.isEven ? AppColors.primaryColor : Colors.white,
+          image: const DecorationImage(
+              image: AssetImage('lib/assets/images/fire.png'))),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:clickandcut/colors.dart';
 import 'package:clickandcut/components/big_text.dart';
+import 'package:clickandcut/components/carousel.dart';
 import 'package:clickandcut/components/small_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,52 +11,56 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: Container(
-        child: Container(
-          margin: const EdgeInsets.only(top: 35),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  children: [
-                    BigText(
-                      text: "India",
-                      FontSize: 20,
-                      color: AppColors.primaryColor,
-                    ),
-                    Row(
-                      children: [
-                        SmallText(
-                          text: "Chennai",
-                          color: Colors.black54,
-                        ),
-                        const Icon(Icons.arrow_drop_down, color: Colors.black54)
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 45,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(15),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 35),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: [
+                      BigText(
+                        text: "India",
+                        FontSize: 20,
+                        color: AppColors.primaryColor,
                       ),
-                      child: Icon(Icons.search, color: Colors.white),
-                    ),
-                  ],
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Chennai",
+                            color: Colors.black54,
+                          ),
+                          const Icon(Icons.arrow_drop_down,
+                              color: Colors.black54)
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          color: AppColors.primaryColor,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Icon(Icons.search, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Carousel(),
+        ],
       ),
     );
   }
