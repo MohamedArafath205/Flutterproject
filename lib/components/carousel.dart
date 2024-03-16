0e3ce0a305a4fe9539ad19c2b457e5a447e3ada3
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-03-16 10:58:31 +0530

    updates on carousel

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -9,11 +9,14 @@ class Carousel extends StatefulWidget {
 }
 
 class _CarouselState extends State<Carousel> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
+        color: Colors.redAccent,
         height: 320,
         child: PageView.builder(
+            controller: pageController,
             itemCount: 5,
             itemBuilder: (context, position) {
               return _buildPageItem(position);
@@ -21,14 +24,30 @@ class _CarouselState extends State<Carousel> {
   }
 
   Widget _buildPageItem(int Index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: Index.isEven ? AppColors.primaryColor : Colors.white,
-          image: const DecorationImage(
-              image: AssetImage('lib/assets/images/fire.png'))),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Index.isEven ? AppColors.primaryColor : Colors.white,
+              image: const DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('lib/assets/images/download.jpeg'))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: const EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.amber,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
